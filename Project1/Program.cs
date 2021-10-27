cb3dc8e68eb25f3413d922e5aa2328fb189a2bee
Alan Dhooghe <s124568@ap.be>
2021-10-27 14:41:18 +0200

    program.cs changed boss

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -14,6 +14,7 @@ namespace Project1
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            Console.Write("hey");
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
