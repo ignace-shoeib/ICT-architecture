d13b3da0ff3f8d00ba5b0cde0873ed09c93da320
kenx00x <kenx00x@gmail.com>
2021-10-27 15:00:12 +0200

    hello

---
+++ Project/Controllers/FileController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Hello()
+        {
+            return Ok("hello");
+        }
+    }
+}
