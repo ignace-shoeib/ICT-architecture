87ca360e9f310aba34f437b7f2b884b86dfedfc5
kenx00x <kenx00x@gmail.com>
2021-11-13 16:05:44 +0100

    file uploads working

--- Project/Controllers/FilesController.cs
+++ Project/Controllers/FilesController.cs
@@ -18,7 +18,12 @@ namespace Project.Controllers
         [HttpPost]
         public async Task<IActionResult> FilesAsync(string fileName, IFormFile file)
         {
-            using (var client = new AmazonS3Client("ASIA5IU4YWEJRNZZDON2", "uQI99fMtnhIib0iVr0U9hmmYzFQQviw4YUUaO0hQ", RegionEndpoint.USEast1))
+            string awsAccessKeyId = "ASIA5IU4YWEJT3RT4XUD";
+            string awsSecretAccessKey = "3WEaFdC8Wsu2sce9Rgh8jSzHdKXJS7WCZwDhe6iE";
+            string awsSessionToken = "FwoGZXIvYXdzEKD//////////wEaDJXm00rBFQBogAntVSLMAR9zU+0V9d+oCCDnaU5Q6Fs9HtkRDfCuHfbFzZBenC1Ocskm8sGT/RB0I1rmKFlenOmijdy1YSyEJtaYmbq0Lee6A74q8tRR6edwXgfcB/mvPh0yzgIb0ODoYmsppgA/pWiXyDxLBljmV7S7qsT3CSCoMco4g5kB5l7GMs34vllnad3+3Z6PriYw+a/4E3PghgQSKysMu3g3RtMah9ZkDkmyqFmtBtaj/54G1rk1FNHXIS9UahvB64cZxMp78XAsOiGZyZ5ETwQsCq5RECirpL+MBjIt++0GdvDYabGPSr600w0u7cetxJMuXjVXDyknbMefs0S0ouGTB66zu2v30+U2";
+            RegionEndpoint region = RegionEndpoint.USEast1;
+            string bucketName = "myaphogeschoolawss3bucket";
+            using (var client = new AmazonS3Client(awsAccessKeyId, awsSecretAccessKey, awsSessionToken, region))
             {
                 using (var newMemoryStream = new MemoryStream())
                 {
@@ -28,7 +33,7 @@ namespace Project.Controllers
                     {
                         InputStream = newMemoryStream,
                         Key = file.FileName,
-                        BucketName = "myaphogeschoolawss3bucket",
+                        BucketName = bucketName,
                         CannedACL = S3CannedACL.PublicRead
                     };
                     var fileTransferUtility = new TransferUtility(client);
