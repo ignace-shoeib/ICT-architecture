fe89e5c0c2cd9377c07cc2d9c47ae07edea2cd3f
kenx00x <kenx00x@gmail.com>
2021-11-17 11:37:14 +0100

    ok

--- Project/Controllers/UserController.cs
+++ Project/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using Amazon;
 using Amazon.CognitoIdentity;
+using Amazon.CognitoIdentityProvider.Model;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
@@ -13,10 +14,17 @@ namespace Project.Controllers
         string awsSecretAccessKey = AWSCredentials.awsSecretAccessKey;
         string awsSessionToken = AWSCredentials.awsSessionToken;
         RegionEndpoint region = AWSCredentials.region;
+        string appClientID = AWSCredentials.appClientID;
         [HttpPost]
         public IActionResult CreateUser(string email, string password)
         {
             AmazonCognitoIdentityClient client = new AmazonCognitoIdentityClient(awsAccessKeyId, awsSecretAccessKey, awsSessionToken, region);
+            var signUpRequest = new SignUpRequest
+            {
+                ClientId = appClientID,
+                Password = password,
+                Username = email,
+            };
             return Created("", "");
         }
     }
