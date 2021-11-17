56125abaf44e022db313e36ad6f1abe865171849
kenx00x <kenx00x@gmail.com>
2021-11-17 11:21:04 +0100

    added cognito client

--- Project/Controllers/UserController.cs
+++ Project/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using Amazon.CognitoIdentity;
+﻿using Amazon;
+using Amazon.CognitoIdentity;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
@@ -8,9 +9,14 @@ namespace Project.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        string awsAccessKeyId = AWSCredentials.awsAccessKeyId;
+        string awsSecretAccessKey = AWSCredentials.awsSecretAccessKey;
+        string awsSessionToken = AWSCredentials.awsSessionToken;
+        RegionEndpoint region = AWSCredentials.region;
         [HttpPost]
         public IActionResult CreateUser(string email, string password)
         {
+            AmazonCognitoIdentityClient client = new AmazonCognitoIdentityClient(awsAccessKeyId, awsSecretAccessKey, awsSessionToken, region);
             return Created("", "");
         }
     }
