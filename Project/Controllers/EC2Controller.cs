cb96a926c08e438ef06d308b3fd7e04675fb1dab
KVGFFN <78799007+KVGFFN@users.noreply.github.com>
2021-11-24 23:29:48 +0100

    ADD instance name

--- Project/Controllers/EC2Controller.cs
+++ Project/Controllers/EC2Controller.cs
@@ -13,7 +13,8 @@ using Amazon.EC2.Model;
 #region BRONNEN
 // AmazonEC2Client:     https://docs.aws.amazon.com/sdkfornet/v3/apidocs/items/EC2/TEC2Client.html
 //                      https://s3.cn-north-1.amazonaws.com.cn/aws-dam-prod/china/pdf/aws-sdk-net-dg.pdf
-
+// Tags - resourcetype  https://sdk.amazonaws.com/java/api/latest/software/amazon/awssdk/services/ec2/model/ResourceType.html#INSTANCE
+// Tags                 https://stackoverflow.com/questions/30337918/example-of-programmatically-creating-restoring-an-ebs-snapshot-using-aws-sdk                     
 #endregion
 
 namespace Project.Controllers
@@ -38,6 +39,24 @@ namespace Project.Controllers
                     MinCount = 1,
                     MaxCount = 1,
                     ImageId = "ami-04902260ca3d33422", // Amazon Linux 2 AMI (HVM) - Kernel 5.10 
+
+                    // Adds name to instance
+                    TagSpecifications = new List<TagSpecification>
+                    {
+                        new TagSpecification()
+                        {
+                            Tags = new List<Tag>
+                            {
+                                {
+                                    new Tag()
+                                    {
+                                        Key = "Name",
+                                        Value = instanceName
+                                    }
+                                }
+                            }, ResourceType = "INSTANCE"
+                        }
+                    }
                 };
 
 
