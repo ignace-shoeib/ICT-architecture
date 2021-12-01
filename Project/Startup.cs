d52f70029e5e93e57f85493bf336fdf8f94c53d3
kenx00x <kenx00x@gmail.com>
2021-12-01 11:42:55 +0100

    THX KAINE

--- Project/Startup.cs
+++ Project/Startup.cs
@@ -15,7 +15,12 @@ using Project.Models;
 using MySql;
 using Pomelo.EntityFrameworkCore.MySql;
 using Microsoft.EntityFrameworkCore;
-
+using Microsoft.IdentityModel.Tokens;
+using System.Net;
+using Newtonsoft.Json;
+#region BRONNEN
+// TOKEN VALIDATION: https://referbruv.com/blog/posts/securing-aspnet-core-apis-with-jwt-bearer-using-aws-cognito     
+#endregion
 namespace Project
 {
     public class Startup
@@ -39,8 +44,11 @@ namespace Project
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Project", Version = "v1" });
             });
+            services.AddAuthentication().AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = GetCognitoTokenValidationParams();
+            });
         }
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -61,6 +69,33 @@ namespace Project
             {
                 endpoints.MapControllers();
             });
+            app.UseAuthentication();
+        }
+        private TokenValidationParameters GetCognitoTokenValidationParams()
+        {
+            var cognitoIssuer = $"https://cognito-idp.{Configuration["region"]}.amazonaws.com/{Configuration["userPoolId"]}";
+            var jwtKeySetUrl = $"{cognitoIssuer}/.well-known/jwks.json";
+            var cognitoAudience = Configuration["appClientId"];
+
+            return new TokenValidationParameters
+            {
+                IssuerSigningKeyResolver = (s, securityToken, identifier, parameters) =>
+                {
+                    // get JsonWebKeySet from AWS
+                    var json = new WebClient().DownloadString(jwtKeySetUrl);
+
+                    // serialize the result
+                    var keys = JsonConvert.DeserializeObject<JsonWebKeySet>(json).Keys;
+
+                    // cast the result to be the type expected by IssuerSigningKeyResolver
+                    return (IEnumerable<SecurityKey>)keys;
+                },
+                ValidIssuer = cognitoIssuer,
+                ValidateIssuerSigningKey = true,
+                ValidateIssuer = true,
+                ValidateLifetime = true,
+                ValidAudience = cognitoAudience
+            };
         }
     }
 }
