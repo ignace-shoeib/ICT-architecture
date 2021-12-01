cf2dc5b7990c93304958ba2ce0a4fd1890369ed8
kenx00x <kenx00x@gmail.com>
2021-12-01 11:48:47 +0100

    wat

--- Project/AWSCredentials.cs
+++ Project/AWSCredentials.cs
@@ -1,5 +1,4 @@
 ﻿using Amazon;
-
 namespace Project
 {
     public class AWSCredentials
@@ -9,12 +8,9 @@ namespace Project
         public static string awsSecretAccessKey = "Zqm7DE1g9x18sOei0dMVzVNscUgOJCYfXlwn0z3m";
         public static string awsSessionToken = "FwoGZXIvYXdzEDsaDEesr1IYi6fCw7IJZiLOAStMpTiuDJWwgHOPvKYlZMHCpfHABHb6qm0Vf5d92Mm2/4h8FwUtVTXBH9LnD2T2z+kz7AC4Cf8i7QICoO5Nt5O37ir+h3LWEWENsI6rRyk1M0stoGkayYv+jw24fEwEniHZsCVLboPldRC3TJfpUhDFxJLM9V9EPPJCbTVnIXyRc+9bDVJ+6lbnIx/5zt4GAIqb1DT+5+wX+MkJWgce7ooXhFhp1N6qMIjqywNHg6n2cOu1qpv+Rbt8nvTDjQMLEVwxiOxWOh9kFvE/rWdkKNnAmY0GMi3FfkDVvwo4yGN/aN0glz+y5uyPcNl5xgT6Hux0eTqYoCCEp4ckDCEUvTKcnHc=";
         public static RegionEndpoint region = RegionEndpoint.USEast1;
-
         // BUCKET ACCESS
         public static string bucketName = "testbucketkaine1";
         public static string appClientId = "6ke8vkca9e4lqhc787orsiocec";
         public static string poolId = "us-east-1_y1Etub0Qe";
-
-
     }
 }
