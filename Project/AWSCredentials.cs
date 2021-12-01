3bf20cb7bb09b066f8b0f8280f33e55386707a09
KVGFFN <78799007+KVGFFN@users.noreply.github.com>
2021-12-01 19:55:43 +0100

    edit RDS (working connection)

--- Project/AWSCredentials.cs
+++ Project/AWSCredentials.cs
@@ -4,9 +4,9 @@ namespace Project
     public class AWSCredentials
     {
         // CLOUD ACCESS
-        public static string awsAccessKeyId = "ASIAXJIMXYDL2EATZL42";
-        public static string awsSecretAccessKey = "Zqm7DE1g9x18sOei0dMVzVNscUgOJCYfXlwn0z3m";
-        public static string awsSessionToken = "FwoGZXIvYXdzEDsaDEesr1IYi6fCw7IJZiLOAStMpTiuDJWwgHOPvKYlZMHCpfHABHb6qm0Vf5d92Mm2/4h8FwUtVTXBH9LnD2T2z+kz7AC4Cf8i7QICoO5Nt5O37ir+h3LWEWENsI6rRyk1M0stoGkayYv+jw24fEwEniHZsCVLboPldRC3TJfpUhDFxJLM9V9EPPJCbTVnIXyRc+9bDVJ+6lbnIx/5zt4GAIqb1DT+5+wX+MkJWgce7ooXhFhp1N6qMIjqywNHg6n2cOu1qpv+Rbt8nvTDjQMLEVwxiOxWOh9kFvE/rWdkKNnAmY0GMi3FfkDVvwo4yGN/aN0glz+y5uyPcNl5xgT6Hux0eTqYoCCEp4ckDCEUvTKcnHc=";
+        public static string awsAccessKeyId = "ASIAXJIMXYDL3OBCPLAG";
+        public static string awsSecretAccessKey = "XAVIqRbU0BvibEN/uoGVh4VlrLtzgF+cZWK/2x6N";
+        public static string awsSessionToken = "FwoGZXIvYXdzEFMaDLsfqoK4/7kmpfNqnyLOAeG/NdCanBaer5QfuJTAiirDQ3vkaPdTYN9KXdgtdBTQ0esx4amL7GO4xKQhHkJ/P04eUxbOoiTZkjKpwo8srcZ8c54DUAXOk0PuNacyU5GnZL8pQ5SXfdZfHfhqc1TfIMWbz4Urs6BuEj3z+reQREuSpguyJGk6x/u6vC++LUzkSbm5uSGExLr0qe3iQWNsD9kPrc5PXr+xSSuRYRwSnbH9v5nh2uTbEbtSarF3MlKrMKLV5uesjKj4W2ENTq9sq0XxXWJNFKscKeV2Ej8jKOvjno0GMi3BQpPHdM7DHOmwdKdZK1g20//rY6QfsCLw2LnfQvxFT4Svk+WHMU+hhcypBy8=";
         public static RegionEndpoint region = RegionEndpoint.USEast1;
         // BUCKET ACCESS
         public static string bucketName = "testbucketkaine1";
--- Project/Controllers/RDSController.cs
+++ Project/Controllers/RDSController.cs
@@ -1,4 +1,5 @@
 ﻿using Amazon;
+using Amazon.RDS;
 using Amazon.S3;
 using Amazon.S3.Transfer;
 using Microsoft.AspNetCore.Http;
@@ -7,10 +8,12 @@ using MySql.Data.MySqlClient;
 using Project.Models;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
 #region BRONNEN
 // Connect MySQL Workbench with RDS: https://stackoverflow.com/questions/16488135/unable-to-connect-mysql-workbench-to-rds-instance
+
 #endregion
 namespace Project.Controllers
 {
@@ -33,53 +36,8 @@ namespace Project.Controllers
         // Create Database FileDB
         // Add Table "Files" containing FileID and FileName
 
-        #region Create FileDB
-        [HttpGet]
-        public IActionResult CreateFileDB()
-        {
-            MySqlConnectionStringBuilder conn_string = new MySqlConnectionStringBuilder();
-            conn_string.Server = "kaine-db.cqftybxhj9nh.us-east-1.rds.amazonaws.com";
-            conn_string.UserID = "admin";
-            conn_string.Password = "rootrootroot";  // weg commenten als je XAMPP gebruikt i guess
-            conn_string.Database = "kaine-db";
-            conn_string.Port = 3306;        // Verander naar 3306 als je geen gehandicapte installatie hebt zoals mij
 
-            List<FileModel> files = new List<FileModel>();
-            using (MySqlConnection conn = new MySqlConnection(conn_string.ToString()))
-            {
-
-                string query = @"
-                CREATE DATABASE IF NOT EXISTS `FileDB`;
-                USE `FileDB`;
-                CREATE TABLE IF NOT EXISTS `Files`
-                (
-                    FileID      INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-                    FileName    VARCHAR(30) NOT NULL
-                );
-                SELECT * FROM `FileDB`.`Files`;
-
-                                ";
-
-                conn.Open();
-                MySqlCommand cmd = new MySqlCommand(query, conn);
-                MySqlDataReader dr = cmd.ExecuteReader();
-
-                while (dr.Read())
-                {
-                    FileModel file = new FileModel();
-                    file.FileID = Convert.ToInt32(dr["File_ID"]);
-                    file.FileName = dr["File_Name"].ToString();
-
-
-                    files.Add(file);
-                }
-                dr.Close();
-            }
-
-            return Ok(files);
-        }
-        #endregion
-
+     
 
         #region Upload FILE + Add to database
         [HttpPost]
@@ -90,6 +48,9 @@ namespace Project.Controllers
             string UUID = "";
             using (var client = new AmazonS3Client(awsAccessKeyId, awsSecretAccessKey, awsSessionToken, region))
             {
+                List<FileModel> files = new List<FileModel>();
+                string filename = "";
+
                 MySqlConnectionStringBuilder conn_string = new MySqlConnectionStringBuilder();
                 conn_string.Server = "127.0.0.1";
                 conn_string.UserID = "root";
@@ -97,39 +58,126 @@ namespace Project.Controllers
                 conn_string.Database = "test";
                 conn_string.Port = 3307;        // Verander naar 3306
 
-                using (MySqlConnection conn = new MySqlConnection(conn_string.ToString()))
+                using (AmazonRDSClient rds = new AmazonRDSClient(awsAccessKeyId, awsSecretAccessKey, awsSessionToken, region))
                 {
-                    using (var newMemoryStream = new MemoryStream())
-                    {
-                        file.CopyTo(newMemoryStream);
 
-                        var uploadRequest = new TransferUtilityUploadRequest
+
+
+                    using (MySqlConnection conn = new MySqlConnection(conn_string.ToString()))
+                    {
+                        using (var newMemoryStream = new MemoryStream())
                         {
-                            InputStream = newMemoryStream,
-                            Key = file.FileName,
-                            BucketName = bucketName,
-                            CannedACL = S3CannedACL.PublicRead
-                        };
-                        UUID = uploadRequest.Key;
-                        var fileTransferUtility = new TransferUtility(client);
-                        await fileTransferUtility.UploadAsync(uploadRequest);
+                            file.CopyTo(newMemoryStream);
+
+                            var uploadRequest = new TransferUtilityUploadRequest
+                            {
+                                InputStream = newMemoryStream,
+                                Key = file.FileName,
+                                BucketName = bucketName,
+                                CannedACL = S3CannedACL.PublicRead
+                            };
+                            UUID = uploadRequest.Key;
+                            var fileTransferUtility = new TransferUtility(client);
+                            await fileTransferUtility.UploadAsync(uploadRequest);
+                        }
+
+                        filename = file.FileName;
+
+                        string query = $@"USE FileDB;INSERT INTO Files (FileName) VALUES ('{filename}');";
+                        using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                        {
+                            try
+                            {
+                                conn.Open();
+                                cmd.ExecuteNonQuery();
+                                Debug.WriteLine($"EXECUTED.");
+                            }
+                            catch (Exception ex)
+                            {
+                                Debug.WriteLine($"ERROR: {ex.ToString()}");
+                            }
+                            finally
+                            {
+                                conn.Close();
+                            }
+                        }
+
+                        
+
+                        /*
+                        try
+                        {
+                            conn.Open();
+                            cmd.ExecuteNonQuery();
+                            Debug.WriteLine($"EXECUTED.");
+                        }
+                        catch (Exception ex)
+                        {
+                            Debug.WriteLine($"ERROR: {ex.ToString()}");
+                        }
+                        finally
+                        {
+                            conn.Close();
+                        }
+                        */
+
+
                     }
                 }
+                
             }
             return Created(UUID, file);
         }
 
         #endregion
 
-        /*
+        
+        // Als connection niet werkt, voeg jouw IP aan security group toe
         [HttpGet]
         public async Task<IActionResult> CheckConnection()
         {
+            MySqlConnectionStringBuilder conn_string = new MySqlConnectionStringBuilder();
+            conn_string.Server = "kaine-db.cqftybxhj9nh.us-east-1.rds.amazonaws.com";
+            conn_string.UserID = "admin";
+            conn_string.Password = "rootrootroot";
+            //conn_string.Database = "kaine-db";
+            conn_string.Port = 3306;
+
             using (AmazonRDSClient rds = new AmazonRDSClient(awsAccessKeyId, awsSecretAccessKey, awsSessionToken, region))
             {
-                
+                List<FileModel> files = new List<FileModel>();
+                using (MySqlConnection conn = new MySqlConnection(conn_string.ToString()))
+                {
+                    string query = @"
+                    CREATE DATABASE IF NOT EXISTS `FileDB`;
+                    USE `FileDB`;
+                    CREATE TABLE IF NOT EXISTS `Files`
+                    (
+                        FileID      INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                        FileName    VARCHAR(30) NOT NULL
+                    );
+                    SELECT * FROM `FileDB`.`Files`;
+                    ";
+
+                    conn.Open();
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    MySqlDataReader dr = cmd.ExecuteReader();
+
+                    while (dr.Read())
+                    {
+                        FileModel file = new FileModel();
+                        file.FileID = Convert.ToInt32(dr["File_ID"]);
+                        file.FileName = dr["File_Name"].ToString();
+
+
+                        files.Add(file);
+                    }
+                    dr.Close();
+                }
+
+                return Ok(files);
             }
         }
-        */       
+       
     }
 }
